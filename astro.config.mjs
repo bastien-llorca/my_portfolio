93223548ad83627cfc1b13f7699ffc421009cb23
bastien-llorca <bastien.llorca@laplateforme.io>
2024-12-09 22:04:32 +0100

    changement

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,7 @@ export default defineConfig({
   integrations: [tailwind()],
   site: "https://github.com/bastien-llorca/Portfolio",
   base: "/Portfolio/", // Nom du dépôt (exemple)
+  outDir: "./dist", // Assurez-vous que le dossier de sortie est correct
+  integrations: [], // Ajoutez les intégrations nécessaires
+  output: "server", // Passez en mode serveur
 });
